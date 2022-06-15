49a3a7058a2409de2070bc2e3358c7ed167afb69
SajjadMazhar <sajjad21@navgurukul.org>
2022-06-15 08:00:59 +0530

    this might work

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(require("cors")())
 // });
 
 // app.use("/static", express.static(path.join(__dirname,"public")))
-app.use("/static",express.static("client/build"))
+app.use(express.static("client/build"))
 app.use('/api/user', require('./routes/user.route'));
 app.use('/api/note', require('./routes/note.route'));
 
